bc8688bb0d093175ca6e01898fcd1969b1acd1cb
Oski <115146064+oskiefitz@users.noreply.github.com>
2024-04-05 20:00:34 +0100

    Update kjv.sh

--- kjv.sh
+++ kjv.sh
@@ -1,26 +1,91 @@
-#!/bin/sh
+#!/bin/bash
+# kjv: Read the Word of God from your terminal
+# License: Public domain
 
 SELF="$0"
 
-# Function to extract data from the script
 get_data() {
     sed '1,/^#EOF$/d' < "$SELF" | tar xzf - -O "$1"
 }
 
-# Function to display help message
-show_help() {
-    # Help message remains the same as provided
-}
-
-# Parse command-line arguments
-while [ $# -gt 0 ]; do
-    # Parse command-line arguments as needed
-done
-
-# Interactive mode
-if [ $# -eq 0 ]; then
-    # Interactive mode logic remains the same as provided
+if [ -z "$PAGER" ]; then
+    if command -v less >/dev/null; then
+        PAGER="less"
+    else
+        PAGER="cat"
+    fi
 fi
 
-# Process non-interactive mode
-get_data output.tsv | awk -v cmd=ref -v ref="$*" "$(get_data kjv.awk)" | less
+show_help() {
+    exec >&2
+    echo "usage: $(basename "$0") [flags] [reference...]"
+    echo
+    echo "  -l      list books"
+    echo "  -W      no line wrap"
+    echo "  -h      show help"
+    echo
+    echo "  Reference types:"
+    echo "      <Book>"
+    echo "          Individual book"
+    echo "      <Book>:<Chapter>"
+    echo "          Individual chapter of a book"
+    echo "      <Book>:<Chapter>:<Verse>[,<Verse>]..."
+    echo "          Individual verse(s) of a specific chapter of a book"
+    echo "      <Book>:<Chapter>-<Chapter>"
+    echo "          Range of chapters in a book"
+    echo "      <Book>:<Chapter>:<Verse>-<Verse>"
+    echo "          Range of verses in a book chapter"
+    echo "      <Book>:<Chapter>:<Verse>-<Chapter>:<Verse>"
+    echo "          Range of chapters and verses in a book"
+    echo
+    exit 2
+}
+
+while [ $# -gt 0 ]; do
+    isFlag=0
+    firstChar="${1%"${1#?}"}"
+    if [ "$firstChar" = "-" ]; then
+        isFlag=1
+    fi
+
+    if [ "$1" = "--" ]; then
+        shift
+        break
+    elif [ "$1" = "-l" ]; then
+        # List all book names with their abbreviations
+        get_data output.tsv | awk -v cmd=list "$(get_data kjv.awk)"
+        exit
+    elif [ "$1" = "-W" ]; then
+        export KJV_NOLINEWRAP=1
+        shift
+    elif [ "$1" = "-h" ] || [ "$isFlag" -eq 1 ]; then
+        show_help
+    else
+        break
+    fi
+done
+
+cols=$(tput cols 2>/dev/null)
+if [ $? -eq 0 ]; then
+    export KJV_MAX_WIDTH="$cols"
+fi
+
+if [ $# -eq 0 ]; then
+    if [ ! -t 0 ]; then
+        show_help
+    fi
+
+    # Interactive mode
+    while true; do
+        printf "kjv> "
+        if ! read -r ref; then
+            break
+        fi
+        get_data output.tsv | awk -v cmd=ref -v ref="$ref" "$(get_data kjv.awk)" | ${PAGER}
+    done
+    exit 0
+fi
+
+get_data output.tsv | awk -v cmd=ref -v ref="$*" "$(get_data kjv.awk)" | ${PAGER}
+
+#EOF
